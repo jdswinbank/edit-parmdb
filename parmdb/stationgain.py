acf89d6178d38c58d10d9c6fc37f0a18a00c49cc
John Swinbank <swinbank@transientskp.org>
2012-03-14 10:36:08 +0100

    Force all NaNs to be zero

--- parmdb/stationgain.py
+++ parmdb/stationgain.py
@@ -7,25 +7,25 @@ class ComplexArray(object):
         self.data = real + 1j * imag
 
     def get_amp(self):
-        return numpy.absolute(self.data)
+        return numpy.absolute(numpy.nan_to_num(self.data))
     def set_amp(self, new_amps):
         self.data = new_amps * self.data / numpy.absolute(self.data)
     amp = property(get_amp, set_amp)
 
     def get_phase(self):
-        return numpy.angle(self.data)
+        return numpy.angle(numpy.nan_to_num(self.data))
     def set_phase(self, new_phase):
         self.data = numpy.vectorize(cmath.rect)(numpy.absolute(self.data), new_phase)
     phase = property(get_phase, set_phase)
 
     def get_real(self):
-        return numpy.real(self.data)
+        return numpy.real(numpy.nan_to_num(self.data))
     def set_real(self, new_real):
         self.data = new_real + 1j * numpy.imag(self.data)
     real = property(get_real, set_real)
 
     def get_imag(self):
-        return numpy.imag(self.data)
+        return numpy.imag(numpy.nan_to_num(self.data))
     def set_imag(self, new_imag):
         self.data = numpy.real(self.data) + 1j * new_imag
     imag = property(get_imag, set_imag)
