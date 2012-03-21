f3d36ce3b928a326fd94a4701bc85656d14e4f23
John Swinbank <swinbank@transientskp.org>
2012-03-21 12:48:36 +0100

    first attempt to zero phases

---
+++ zero_phase.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import sys
+import numpy
+from parmdb.stationgain import StationGain
+from edit_parmdb import list_stations
+
+if __name__ == "__main__":
+    pdbfile = sys.argv[1]
+    stations = list_stations(pdbfile)
+    for station in stations:
+        stationgain = StationGain(pdbfile, station)
+        for value in stationgain.itervalues():
+            value.phase = value.phase * 0
+        stationgain.writeout()
