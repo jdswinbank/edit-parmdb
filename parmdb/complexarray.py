f5b9597ef988593dac6969af1cff5f6c8320fbc6
John Swinbank <swinbank@transientskp.org>
2012-03-26 10:22:41 +0200

    Accept lists as well as numpy arrays as arguments

--- parmdb/complexarray.py
+++ parmdb/complexarray.py
@@ -8,28 +8,30 @@ class ComplexArray(object):
     def get_amp(self):
         return numpy.absolute(numpy.nan_to_num(self.data))
     def set_amp(self, new_amps):
-        self.data = new_amps * self.data / numpy.absolute(self.data)
+        self.data = numpy.array(new_amps) * self.data / numpy.absolute(self.data)
     amp = property(get_amp, set_amp)
     Ampl = amp
 
     def get_phase(self):
         return numpy.angle(numpy.nan_to_num(self.data))
     def set_phase(self, new_phase):
-        self.data = numpy.vectorize(cmath.rect)(numpy.absolute(self.data), new_phase)
+        self.data = numpy.vectorize(cmath.rect)(
+            numpy.absolute(self.data), numpy.array(new_phase)
+        )
     phase = property(get_phase, set_phase)
     Phase = phase
 
     def get_real(self):
         return numpy.real(numpy.nan_to_num(self.data))
     def set_real(self, new_real):
-        self.data = new_real + 1j * numpy.imag(self.data)
+        self.data = numpy.array(new_real) + 1j * numpy.imag(self.data)
     real = property(get_real, set_real)
     Real = real
 
     def get_imag(self):
         return numpy.imag(numpy.nan_to_num(self.data))
     def set_imag(self, new_imag):
-        self.data = numpy.real(self.data) + 1j * new_imag
+        self.data = numpy.real(self.data) + 1j *  numpy.array(new_imag)
     imag = property(get_imag, set_imag)
     Imag = imag
 
@@ -40,11 +42,13 @@ class ComplexArray(object):
 class RealImagArray(ComplexArray):
     keys = ("Real", "Imag")
     def __init__(self, real, imag):
-        self.data = real + 1j * imag
+        self.data = numpy.array(real) + 1j * numpy.array(imag)
 
 class AmplPhaseArray(ComplexArray):
     keys = ("Ampl", "Phase")
     def __init__(self, ampl, phase):
-        self.data = numpy.vectorize(cmath.rect)(ampl, phase)
+        self.data = numpy.vectorize(cmath.rect)(
+            numpy.array(ampl), numpy.array(phase)
+        )
 
 ARRAY_TYPES = [RealImagArray, AmplPhaseArray]
